4310ef3b4965888e080b0d79ae397a32838ddab7
tsipporahc <tsipporahc@gmail.com>
2022-09-07 12:17:50 -0700

    showPage and addPagination functions display properly

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -16,15 +16,71 @@ For assistance:
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
+
+list - array of student data
+page - page number
 */
 
+function showPage (list, page) {
+   const startIndex = (parseInt(page) * 9) - 9; //the index for the first student on the page
+   const endIndex = parseInt(page) * 9; //the index for the last student on the page
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML =''; // removes any students that previously displayed
+
+  for (let i=0; i<list.length; i++) {
+      if (endIndex >= i >= startIndex) {
+         const studentItem = `
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email"> ${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>
+         `;
+         studentList.insertAdjacentHTML("beforeend", studentItem);
+         
+      }
+   } 
+   return studentList;
+
+}
+showPage (data, 1);
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
+
+list - student data
 */
 
+function addPagination (list) {
+   const numButton = Math.ceil(list.length / 9) ; //store the value of the number of pagination buttons needed. 
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = ''; // remove any pagination buttons previously displayed
+   console.log(numButton);
+   for (let i=1; i < numButton + 1 ; i++) {
+        /*  const button = document.createElement('button');
+         button.textContent = `${i}`;
+         linkList.appendChild(button); // attatch button to link list */
 
+
+         const pageNumber = `
+         <li>
+          <button type="button">${i}</button>
+         </li>
+         `;
+         linkList.insertAdjacentHTML("beforeend", pageNumber);
+
+      }
+      
+
+}
+
+addPagination (data);
 
 // Call functions
